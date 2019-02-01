3ca72d9724b629f7a6c6c55be6d9267733d1dff1
Langdon <lfroker@gmail.com>
2019-02-01 09:18:25 -0800

    works?

--- routes/api/events.js
+++ routes/api/events.js
@@ -6,9 +6,7 @@ const multer = require(`multer`);
 
 
 var storage = multer.diskStorage({
-        destination: function (req, file, cb) {
-          cb(null, 'uploads')
-        },
+        destination: '/uploads/',
         filename: function (req, file, cb) {
           cb(null, file.fieldname + '-' + Date.now()+'.jpg')
         }
