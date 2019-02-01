6d8117f8a5320d12e19d4ed3391711e366a090be
Langdon <lfroker@gmail.com>
2019-01-31 21:02:40 -0800

    this one broke the least

--- server.js
+++ server.js
@@ -52,8 +52,13 @@ app.use("/api/users", users);
 app.use("/api/events", events);
 
 
+
+
+
+
 const port = process.env.PORT || 3001;
 if(process.env.NODE_ENV === 'production'){
+  
 
   app.get('*', (req, res)=>{
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
