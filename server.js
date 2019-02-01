3fe6316f1a71b5191aad1fa3b1bda123af6d352b
Langdon <lfroker@gmail.com>
2019-01-31 20:40:15 -0800

    last try

--- server.js
+++ server.js
@@ -19,7 +19,8 @@ app.use(
   })
 );
 
-app.use(express.static('public'))
+// app.use(express.static('public'))
+app.use(express.static(path.join(__dirname, "client", "build")))
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
@@ -50,19 +51,14 @@ require("./config/passport")(passport);
 app.use("/api/users", users);
 app.use("/api/events", events);
 
+
+const port = process.env.PORT || 3001;
 if(process.env.NODE_ENV === 'production'){
-  app.use(express.static('client/build'));
 
   app.get('*', (req, res)=>{
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
   });
 }
-
-
-
-
-const port = process.env.PORT || 3001;
-
 
 
 app.listen(port, () => console.log(`Server started in port ${port}`))
